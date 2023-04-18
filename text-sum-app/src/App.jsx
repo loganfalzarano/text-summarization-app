5ee6500f77433162b8392e9a78dab4149ade0b4e
loganfalzarano <lfalzar1@jhu.edu>
2023-04-18 13:37:43 -0400

    more work

--- text-sum-app/src/App.jsx
+++ text-sum-app/src/App.jsx
@@ -1,26 +1,51 @@
-import { MantineProvider, Grid, Title, Flex, Textarea, Select } from '@mantine/core';
-
+import axios from 'axios';
+import { MantineProvider, Grid, Title, Flex, Textarea, Select, Button, Text, Card, Divider, Space } from '@mantine/core';
 import { ColorSchemeProvider } from '@mantine/core';
 import { React, useState, useEffect } from 'react';
 import ThemeButton from './components/ThemeButton';
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: "org-yjieycH8HCAB02QhNdBXbaYL",
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+delete configuration.baseOptions.headers['User-Agent'];
+const openai = new OpenAIApi(configuration);
+
 
 export default function App() {
   const [colorScheme, setColorScheme] = useState('dark');
   const [value, setValue] = useState('');
+  const [summary, setSummary] = useState('');
 
   useEffect(() => {
     let ignore = false;
-    
+
     if (!ignore) {
       navigator.clipboard.readText().then(text => {
         setValue(text);
       });
     }
     return () => { ignore = true; }
-    },[]);
+  }, []);
 
-  
+  const getSummary = async () => {
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: `Summarize the following text: ${value}`,
+    //   temperature: 0,
+    //   max_tokens: 64,
+    //   top_p: 1.0,
+    //   frequency_penalty: 0.0,
+    //   presence_penalty: 0.0,
+    // });
+    // console.log(response);
+    // setSummary(response.data.choices[0].text);
 
+    const response = await openai.listModels();
+    setSummary(JSON.stringify(response.data.data));
+    console.log(response.data.data);
+  }
 
   return (
     <ColorSchemeProvider>
@@ -56,8 +81,17 @@ export default function App() {
               ]}
               style={{ padding: "10px" }}
             />
-            <Title order={2} align="center" style={{padding:"50px"}}>Insert More Feilds Here</Title>
-            <Title order={2} align="center" style={{padding:"50px"}}>Display The Summary From Trained Model Here</Title>
+            <Title order={2} align="center" style={{ padding: "50px" }}>Insert More Feilds Here</Title>
+            <Title order={2} align="center" style={{ padding: "50px" }}>Display The Summary From Trained Model Here</Title>
+            <Button variant="light" fullWidth style={{padding: "10px"}} onClick={() => {getSummary()}}>
+              Get Summary
+            </Button>
+            <Space style={{padding: "10px"}} />
+            <Divider style={{padding: "10px"}} />
+            <Card withBorder style={{overflowWrap: "break-word", hyphens: "manual", padding:"10px"}} >
+              {summary}
+            </Card>
+            
           </Grid.Col>
           <Grid.Col span="auto">
 
