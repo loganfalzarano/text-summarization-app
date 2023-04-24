1dcc02207a5e2ffc929de2fe6894a8d64b06fc29
loganfalzarano <lfalzar1@jhu.edu>
2023-04-23 23:36:26 -0400

    adding input state

--- text-sum-app/src/App.jsx
+++ text-sum-app/src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { MantineProvider, Grid, Title, Flex, Textarea, Select, Button, Text, Card, Divider, Space } from '@mantine/core';
+import { MantineProvider, Grid, Title, Flex, Textarea, Select, Button, Text, Card, Divider, Space, Slider, Checkbox } from '@mantine/core';
 import { ColorSchemeProvider } from '@mantine/core';
 import { React, useState, useEffect } from 'react';
 import ThemeButton from './components/ThemeButton';
@@ -12,11 +12,21 @@ const configuration = new Configuration({
 delete configuration.baseOptions.headers['User-Agent'];
 const openai = new OpenAIApi(configuration);
 
+const MARKS = [
+  { value: 0, label: '0' },
+  { value: 150, label: '150' },
+  { value: 300, label: '300' },
+  { value: 450, label: '450' },
+];
 
 export default function App() {
-  const [colorScheme, setColorScheme] = useState('dark');
+  const [colorScheme, setColorScheme] = useState('light');
   const [value, setValue] = useState('');
   const [summary, setSummary] = useState('');
+  const [style, setStyle] = useState('');
+  const [level, setLevel] = useState('');
+  const [length, setLength] = useState(300);
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     let ignore = false;
@@ -30,21 +40,32 @@ export default function App() {
   }, []);
 
   const getSummary = async () => {
-    // const response = await openai.createCompletion({
-    //   model: "text-davinci-003",
-    //   prompt: `Summarize the following text: ${value}`,
-    //   temperature: 0,
-    //   max_tokens: 64,
-    //   top_p: 1.0,
-    //   frequency_penalty: 0.0,
-    //   presence_penalty: 0.0,
-    // });
-    // console.log(response);
-    // setSummary(response.data.choices[0].text);
+    const prompt = `Act as: Summarizer
+    Degree of summarization: Determine the field and include what is relevant to it
+    Change style to: ${style}
+    Change tone to: Analytical
+    Change reader comprehension level to: ${level}
+    Change length to: ${length} Words
+    ${checked ? "Then provide me with a series of quizzes or tests on [topic or skill] at varying difficulty levels to enhance my retention and understanding." : ""}
+    Text: 
+    
+    ${value}`;
 
-    const response = await openai.listModels();
-    setSummary(JSON.stringify(response.data.data));
-    console.log(response.data.data);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    console.log(response);
+    setSummary(response.data.choices[0].text);
+
+    // const response = await openai.listModels();
+    // setSummary(JSON.stringify(response.data.data));
+    // console.log(response.data.data);
   }
 
   return (
@@ -59,7 +80,7 @@ export default function App() {
           <Grid.Col span="auto">
           </Grid.Col>
           <Grid.Col span={8}>
-            <Title order={1} align="center" style={{padding:"10px"}}>Text Summarization App</Title>
+            <Title order={1} align="center" style={{ padding: "10px" }}>Text Summarization App</Title>
             <Textarea
               label="Enter what you want summarized"
               placeholder="Paste your text here"
@@ -71,27 +92,59 @@ export default function App() {
               style={{ padding: "10px" }}
             />
             <Select
-              label="Your favorite framework/library"
+              label="Choose your desired tone for the summarization"
               placeholder="Pick one"
               data={[
-                { value: 'react', label: 'React' },
-                { value: 'ng', label: 'Angular' },
-                { value: 'svelte', label: 'Svelte' },
-                { value: 'vue', label: 'Vue' },
+                { value: 'academic', label: 'Academic' },
+                { value: 'PhD Work', label: 'PhD Work' },
+                { value: 'converstional', label: 'Converstional' },
               ]}
               style={{ padding: "10px" }}
+              value={style}
+              onChange={setStyle}
             />
-            <Title order={2} align="center" style={{ padding: "50px" }}>Insert More Fields Here</Title>
-            <Title order={2} align="center" style={{ padding: "50px" }}>Display The Summary From Trained Model Here</Title>
-            <Button variant="light" fullWidth style={{padding: "10px"}} onClick={() => {getSummary()}}>
+            <Select
+              label="Choose your desired level of reader comprehension for the summarization"
+              placeholder="Pick one"
+              data={[
+                { label: 'Expert', value: 'expert, assume extensive prior knowledge on the field and on what is being presented' },
+                { label: 'Experienced', value: 'experienced, assume extensive prior knowledge on the field, but minimal on what is being presented' },
+                { label: 'Beginner', value: 'beginner, assume minimal prior knowledge on the field, and minimal on what is being presented' },
+              ]}
+              style={{ padding: "10px" }}
+              value={level}
+              onChange={setLevel}
+            />
+            <Text size={"sm"} style={{ padding: "10px" }}>Choose the length of the summary</Text>
+            <Slider
+              //style={{ paddingLeft: "18px" , paddingRight: "18px" , paddingBottom: "20px"}}
+              style={{ marginLeft: "10px" , marginRight: "10px" , marginBottom: "20px"}}
+              min={100}
+              max={500}
+              labelTransition="skew-down"
+              labelTransitionDuration={150}
+              labelTransitionTimingFunction="ease"
+              value={length}
+              onChange={setLength}
+            />
+            <Checkbox 
+            label="Create a quiz on the content"
+            style={{ paddingLeft: "10px" , paddingRight: "10px" , paddingBottom: "20px"}}
+            checked={checked} 
+            onChange={(event) => setChecked(event.currentTarget.checked)} 
+            />
+            <Space style={{ padding: "2px" }} />
+            {/* <Title order={2} align="center" style={{ padding: "50px" }}>Insert More Fields Here</Title> */}
+            {/* <Title order={2} align="center" style={{ padding: "50px" }}>Display The Summary From Trained Model Here</Title> */}
+            <Button variant="light" fullWidth style={{ padding: "10px" }} onClick={() => { getSummary() }}>
               Get Summary
             </Button>
-            <Space style={{padding: "10px"}} />
-            <Divider style={{padding: "10px"}} />
-            <Card withBorder style={{overflowWrap: "break-word", hyphens: "manual", padding:"10px"}} >
+            <Space style={{ padding: "10px" }} />
+            <Divider style={{ padding: "10px" }} />
+            <Card withBorder style={{ overflowWrap: "break-word", hyphens: "manual", padding: "10px" }} >
               {summary}
             </Card>
-            
+
           </Grid.Col>
           <Grid.Col span="auto">
 
